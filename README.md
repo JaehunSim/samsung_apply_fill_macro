c62d38d690f7b009319b197003d99e3d7c624ab1
tapu1125 <tapu1125@gmail.com>
2020-09-08 07:30:24 +0900

    Add config

--- README.md
+++ README.md
@@ -5,17 +5,21 @@
 
 # How to use
 
-1. data_sample에 맞게 성적 엑셀 입력
-2. data.xlsx로 파일명 수정
-3. run.py 실행
-4. 3초내로 이수교과목 페이지 과정탭에서 열어주고 엔터
+1. python3.6 설치. 예시링크(https://winpython.github.io/)
+
+2. pip install -r requirements.txt
+
+3. data_sample에 맞게 성적 엑셀 입력
+
+4. data.xlsx로 파일명 수정 또는 코드 수정(10번째 줄)
+
+5. run.py 실행
+
+6. 3초내로 이수교과목 페이지 과정탭에서 열어주고 엔터
+
+7. 네트워크 지연속도나 안정성을 고려한다면 9번째줄 delay_time 조절
 
 ---
-4번 과정 이미지
+6번 과정 이미지
 ![](docs/1.PNG)  
-![](docs/2.PNG) 
-
-link:  https://jaehunsim.tistory.com/40
-
-src: https://github.com/JaehunSim/samsung_apply_fill_macro
-
+![](docs/2.PNG)  
--- run.py
+++ run.py
@@ -3,34 +3,48 @@ import time
 
 import pandas as pd
 import pyperclip
+import yaml
 
 import macro
-delay_time = 0.25 #seconds
+
+def load_config():
+    with open('config.yaml', encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = load_config()
+delay_time = config["delay_time"]
 bot = macro.ChatMacro(delay_time)
 data = pd.read_excel("data.xlsx")
-start_num = 0 #첫번째를 0으로 기준, 중간에 끊기면 이 숫자 조절
+start_num = config["start_num"] #첫번째를 0으로 기준, 중간에 끊기면 이 숫자 조절
+setup_delay_time = config["setup_delay_time"]
+class_taken_year = config["class_taken_year"]
+degree_type = config["degree_type"]
 
 def next_form(bot):
     bot.press("tab")
     bot.press("tab")
     bot.press("enter")
 
-time.sleep(3) #3초내로 과정 탭 누르고 엔터클릭
+time.sleep(setup_delay_time) #3초내로 과정 탭 누르고 엔터클릭
 for index, row in data[start_num:].iterrows():
     bot.press("enter") #과정
+    for i in range(degree_type-1):
+        bot.press("down_arrow")
     bot.press("enter")
     next_form(bot)
     time.sleep(0.5) #전공명 서버 딜레이
     bot.press("enter") #전공명
     next_form(bot)
     year = row["수강년도"] #수강년도
-    count = 2019 - year
+    count = class_taken_year - year
     for i in range(count):
         bot.press("down_arrow")
     bot.press("enter")
     next_form(bot)
     semester = row["학기"] #학기
     if isinstance(semester, int):
+        
         count = semester - 1
     elif semester.startswith("여름"):
         count = 3
@@ -94,8 +108,9 @@ for index, row in data[start_num:].iterrows():
     bot.press("enter")
     next_form(bot) #추가버튼
     if index%10 == 9: #10단위 중간 저장
-        time.sleep(0.5) #딜레이
-        bot.press("enter") #중간 저장 팝업 
+        time.sleep(1) #딜레이
+        bot.press("enter") #중간 저장 팝업
+        time.sleep(1)
     time.sleep(0.2) #추가버튼 지연
     bot.press("tab")
     bot.press("enter") #내용삭제
